4022fa978e9541a561422ecb47e1ceecb3ffebaf
kamphee <qserver52@gmail.com>
2018-07-04 17:36:51 +0700

    แก้ไข

--- ex.php
+++ ex.php
@@ -1,3 +1,15 @@
+<!-- กรอบ -->
+<div class="row">
+    <div class="box col-md-12">
+        <div class="box-inner">
+            <div class="box-header well">
+                <h2><i class="glyphicon glyphicon-info-sign"></i> Introduction</h2>
+
+            </div>
+            <div class="box-content row">
+                <div class="col-lg-7 col-md-12">
+                    <!-- ข้อความ -->
+					
 <h3>ตัวอย่างการออกแบบฐานข้อมูล (Data dictionary)</h3><br>
 <center>
 <iframe title="Include gadget (iframe)" width="800" height="480" scrolling="no" frameborder="0" id="789695664" name="789695664" allowtransparency="true" class="igm" src="file/datadic.pdf">
@@ -17,3 +29,11 @@
 <iframe title="Include gadget (iframe)" width="800" height="480" scrolling="no" frameborder="0" id="789695664" name="789695664" allowtransparency="true" class="igm" src="file/ออกแบบเว็บเพจ.pdf">
 </iframe>
 </center>
+
+<!-- จบข้อความ -->
+                <!-- Ads, you can remove these -->
+				</div>
+        </div>
+    </div>
+</div>
+<!-- จบกรอบ -->
